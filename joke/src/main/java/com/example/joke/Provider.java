44806e83e5b7b85ab7e4c9b7719e82fd8fc73710
Leopold <leopold.chmielewski@gmail.com>
2018-06-05 19:25:41 +0100

    Added joke

--- joke/src/main/java/com/example/joke/Provider.java
+++ joke/src/main/java/com/example/joke/Provider.java
@@ -1,4 +1,10 @@
 package com.example.joke;
 
 public class Provider {
+    public String getJoke() {
+
+        return "Why do they call it hyper text? \n" +
+                "Too much JAVA.";
+    }
+
 }
