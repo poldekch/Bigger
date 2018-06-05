781f519aecb33dd539375fdbecf4942a383a5e04
Leopold <leopold.chmielewski@gmail.com>
2018-06-05 19:40:34 +0100

    Added joke viewer and provider returns the joke

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.joke.Provider;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Provider jokeProvider = new Provider();
+        String joke = jokeProvider.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
 
